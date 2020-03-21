9d6580e2926ea9e69d7002271acc54899b8029b2
blurry-x-face <rishabhshukla1999@gmail.com>
2020-03-21 17:56:08 +0530

    keys

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  clientID:
+    "428185779294-4bue4hiho7mqp2866jqs9vnugv7uskan.apps.googleusercontent.com",
+  clientSecret: "zCMmVUg_jPUZfztsJMxPVZ-m",
+  mongoURI:
+    "mongodb+srv://blurry:KGKhw20qjSc5QZ82@cluster0-fiwnf.mongodb.net/test?retryWrites=true&w=majority",
+    sessionSecret: 'abc23'
+};
